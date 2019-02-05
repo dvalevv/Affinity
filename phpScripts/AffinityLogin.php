36fe90b4ca02d096c00fd68f095860e7316af6a0
Jason Ozuzu <jason.ozuzu@student.manchester.ac.uk>
2019-02-05 14:24:59 +0000

    Changed the filepath for 'include'

--- phpScripts/AffinityLogin.php
+++ phpScripts/AffinityLogin.php
@@ -3,7 +3,7 @@ for possible methods of implementation:
 https://stackoverflow.com/questions/11958243/button-that-runs-a-php-script-without-changing-current-page
 using hidden frames seems to be simplest method so consider using -->
 <?php
-include '/home/h51205jo/Affinity/SQL_Scripts/php_queries'; // Vlad's query file is imported
+include '../SQL_Scripts/php_queries'; // Vlad's query file is imported
 session_start();
 $username = $password = "";
 
--- phpScripts/AffinityRegistration.php
+++ phpScripts/AffinityRegistration.php
@@ -3,7 +3,7 @@ for possible methods of implementation:
 https://stackoverflow.com/questions/11958243/button-that-runs-a-php-script-without-changing-current-page
 using hidden frames seems to be simplest method so consider using -->
 <?php
-include '/home/h51205jo/Affinity/SQL_Scripts/php_queries'; // Vlad's query file is imported
+include '../Affinity/SQL_Scripts/php_queries'; // Vlad's query file is imported
 $username = $password = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")       // The data is retrieved from the source and is treated for sq1
