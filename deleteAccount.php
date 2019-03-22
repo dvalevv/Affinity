4f4c13e7e35273dd201c3def5ee368336e4240fc
Vlad Iacob <vlad.iacob@student.manchester.ac.uk>
2019-03-22 14:18:16 +0000

    some more stuff

--- deleteAccount.php
+++ deleteAccount.php
@@ -12,22 +12,35 @@ if($_POST['cDeleteAcc'] == 'confirm')
 
    while($row = $data->fetch_assoc())
    {
-      $eData = getEventData($row);
-      if($eData['Master'] == $username)
+      $eData = getEventData($row["Event_ID"]);
+      if(strcmp($eData['Master'], $username) == 0)
          array_push($events, $row["Event_ID"]);
       else
-         array_push($eventsP, $row["Event_ID"])
+         array_push($eventsP, $row["Event_ID"]);
    }
-  
+
    for($i = 0; $i < count($eventsP); $i++)
       removeAParticipation($username, $eventsP[$i]);
 
    for($i = 0; $i < count($events); $i++)
       deleteEvent($events[$i]);
 
+
+   $mistedEvents = array();
+   $eventData = getAllEventID();
+   while($row = $eventData->fetch_assoc())
+   {
+      $eData = getEventData($row["Event_ID"]);
+      if(strcmp($eData['Master'], $username) == 0)
+         array_push($mistedEvents, $row["Event_ID"]);
+   }
+
+   for($i = 0; $i < count($mistedEvents); $i++)
+      deleteEvent($mistedEvents[$i]);
+
    deleteAllObjectsFromLikes($username);
    
    deleteUser($username);
 }
-echo '<script language="javascript"> window.location.href = "index.php";</script>';
+echo '<script language="javascript"> window.location.href = "AffinityLogout.php";</script>';
 ?>
