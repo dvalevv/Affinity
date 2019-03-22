741715bd423f0d2a3f6400f16211724d7782f33d
Vlad Iacob <vlad.iacob@student.manchester.ac.uk>
2019-03-22 12:50:34 +0000

    don't ask

--- eventCreation.php
+++ eventCreation.php
@@ -1,4 +1,6 @@
-<?php
-createEvent($master, $expirationDate, $visibility, $name,
-                           $location, $description);
+<?php session_start();
+include 'php_queries.php';
+createEvent($_SESSION['username'], $_POST['expirationDate'], $_POST['publicity'], $_POST['name'],
+            $_POST['location'], $_POST['description']);
+echo '<script language="javascript"> window.location.href = "index.php";</script>';
 ?>
