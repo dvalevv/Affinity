e34598bab1073af03a7d3b5c9128f5cd3c9041ea
Benjamin Rimmer <benjamin.rimmer-2@student.manchester.ac.uk>
2019-03-22 14:46:20 +0000

    working on eventoptions resizing

--- eventoptions.php
+++ eventoptions.php
@@ -42,13 +42,13 @@
 
   
   <div class="groupBox">
-       <div style="margin-top:60px; position: absolute; transform: translateX(-400px);">
+       <div style="margin-top:60px; position: absolute; transform: translateX(-500px);">
             <a href="manageevents.php"><img src="img/manageevents.png">
        </div>
-       <div style="margin-top:60px; position: absolute; transform: translateX(-0px);">
+       <div style="margin-top:60px; position: absolute; transform: translateX(-100px);">
             <a href="createevent.php"><img src="img/createevent.png">
        </div>
-       <div style="margin-top:60px; position: absolute; transform: translateX(400px);">
+       <div style="margin-top:60px; position: absolute; transform: translateX(300px);">
             <a href="events.php"><img src="img/joinevent.png">
        </div>
   </div>
